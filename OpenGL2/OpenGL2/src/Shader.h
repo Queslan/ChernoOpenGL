e04dea4d65bf65ad7e009954e9ffa15bbd64d7ee
Wojciech Sobocinski <sobota96@wp.pl>
2018-07-12 17:36:48 +0200

    Maths
    Episode 19 ended.
    Added extra lib for opengl maths.
    Added aspect ratio for window .

--- OpenGL2/OpenGL2/src/Shader.h
+++ OpenGL2/OpenGL2/src/Shader.h
@@ -1,6 +1,7 @@
 #pragma once
 #include <string>
 #include <unordered_map>
+#include "glm/glm.hpp"
 
 struct ShaderProgramSource {
 	std::string vertexSource;
@@ -16,6 +17,7 @@ public:
 	void Unbind() const;
 
 	void SetUniform4f(const std::string& name, float v0, float v1, float v2, float v3);
+	void SetUniformMat4f(const std::string& name, glm::mat4& matrix);
 	void SetUniform1i(const std::string& name, int value);
 private:
 	unsigned int m_RenderId;
