c9efcdd9553c30efd56e092282b84c3b52886363
Wojciech Sobocinski <sobota96@wp.pl>
2018-07-13 14:07:30 +0200

    Problem with ImGui
    Episodes 20, 21 ended.
    Problem with implementation of ImGui with OpenGL3 functions.

--- OpenGL2/OpenGL2/src/Application.cpp
+++ OpenGL2/OpenGL2/src/Application.cpp
@@ -1,4 +1,5 @@
 // docs.gl - site with openGL commands description
+
 #include <GL/glew.h>
 #include <GLFW/glfw3.h>
 
@@ -18,6 +19,9 @@
 
 #include "glm/glm.hpp"
 #include "glm/gtc/matrix_transform.hpp"
+#include "imgui/imgui_impl_opengl3.h"
+#include "imgui/imgui.h"
+#include "imgui/imgui_impl_glfw.h"
 
 
 int main(void) {
@@ -28,8 +32,12 @@ int main(void) {
 		return -1;
 
 	
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
 	/* Create a windowed mode window and its OpenGL context */
-	window = glfwCreateWindow(777, 777, "777", NULL, NULL);
+	window = glfwCreateWindow(960, 560, "777", NULL, NULL);
 	if (!window) {
 		glfwTerminate();
 		return -1;
@@ -44,14 +52,15 @@ int main(void) {
 		std::cout << "glewInit ERROR" << '\n';
 
 	std::cout << glGetString(GL_VERSION) << '\n'; // show used version of opengl
-	
-	{ // Extra scope for variables to be deleted from stack
+
+	/* Extra scope for variables to be deleted from stack */
+	{ 
 		//Positions of vertices
 		float positions[] {
-			-0.5f, -0.5f, 0.0f, 0.0f, // 0
-			 0.5f, -0.5f, 1.0f, 0.0f, // 1
-			 0.5f,	0.5f, 1.0f, 1.0f, // 2
-			-0.5f,  0.5f, 0.0f, 1.0f  // 3
+			 100.0f, 100.0f, 0.0f, 0.0f, // 0
+			 200.0f, 100.0f, 1.0f, 0.0f, // 1
+			 200.0f, 200.0f, 1.0f, 1.0f, // 2
+			 100.0f, 200.0f, 0.0f, 1.0f  // 3
 		};
 
 		unsigned int indices[] {
@@ -69,12 +78,15 @@ int main(void) {
 
 		IndexBuffer ib(indices, 6);
 		
-		glm::mat4 proj = glm::ortho(-2.0f, 2.0f, -1.5f, 1.5f, -1.0f, 1.0f);
+		glm::mat4 proj = glm::ortho(0.0f, 960.0f, 0.0f, 560.0f, -1.0f, 1.0f);
+		glm::mat4 view = glm::translate(glm::mat4(1.0f), glm::vec3(-100, 0, 0));
+		glm::mat4 model = glm::translate(glm::mat4(1.0f), glm::vec3(200, 200, 0));
 
+		glm::mat4 mvp = proj * view * model;
 		Shader shader("res/shaders/Basic.shader");
 		shader.Bind();
 		shader.SetUniform4f("u_Color", 0.8f, 0.3f, 0.8f, 1.0f);
-		shader.SetUniformMat4f("u_MVP", proj);
+		shader.SetUniformMat4f("u_MVP", mvp);
 
 		Texture texture("res/textures/img.png");
 		texture.Bind();
@@ -87,6 +99,15 @@ int main(void) {
 		shader.Unbind();
 		
 		Renderer renderer;
+
+		ImGui::CreateContext();
+		ImGui_ImplGlfw_InitForOpenGL(window, true);
+		ImGui::StyleColorsDark();
+
+		bool show_demo_window = true;
+		bool show_another_window = false;
+		ImVec4 clear_color = ImVec4(0.45f, 0.55f, 0.60f, 1.00f);
+
 		float r = 0.0f;
 		float changeValue = 0.05f;
 	
@@ -94,6 +115,7 @@ int main(void) {
 		while (!glfwWindowShouldClose(window)) {
 			/* Render here */
 			GLCall(glClear(GL_COLOR_BUFFER_BIT));
+			ImGui_ImplOpenGL3_NewFrame();
 
 			shader.Bind();	
 			shader.SetUniform4f("u_Color", r, 0.3f, 0.8f, 1.0f);
@@ -108,7 +130,26 @@ int main(void) {
 
 			r += changeValue;
 
+			{
+				static float f = 0.0f;
+				static int counter = 0;
+				ImGui::Text("Hello, world!");                           // Display some text (you can use a format string too)
+				ImGui::SliderFloat("float", &f, 0.0f, 1.0f);            // Edit 1 float using a slider from 0.0f to 1.0f    
+				ImGui::ColorEdit3("clear color", (float*)&clear_color); // Edit 3 floats representing a color
 
+				ImGui::Checkbox("Demo Window", &show_demo_window);      // Edit bools storing our windows open/close state
+				ImGui::Checkbox("Another Window", &show_another_window);
+
+				if (ImGui::Button("Button"))                            // Buttons return true when clicked (NB: most widgets return true when edited/activated)
+					counter++;
+				ImGui::SameLine();
+				ImGui::Text("counter = %d", counter);
+
+				ImGui::Text("Application average %.3f ms/frame (%.1f FPS)", 1000.0f / ImGui::GetIO().Framerate, ImGui::GetIO().Framerate);
+			}
+
+			ImGui::Render();
+			ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
 			/* Swap front and back buffers */
 			glfwSwapBuffers(window);
 
@@ -116,6 +157,8 @@ int main(void) {
 			glfwPollEvents();
 		}
 	}
+	ImGui_ImplOpenGL3_Shutdown();
+	ImGui::DestroyContext();
 	glfwTerminate();
 	return 0;
 }
